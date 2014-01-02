8ee18a34f6543e4a4c9bb3832c01155cfd07207c
Charlotte Chang <yellowspiderlove@gmail.com>
2014-01-02 14:23:21 -0500

    added comma and space code

--- app/people.rb
+++ app/people.rb
@@ -1,31 +1,168 @@
+
+##collection
+# put data hashes into people hash in process (figure out what the syntax is)
+# refactor how respective data gets into the peoplehash
+# combine above steps 
+## make peoplehash new people
+
+# space.txt
+
+# method display input - three methods, internal method that calls filename
+# output display
+
+
+require 'date'
+
 class PeopleData
   def initialize
   end
 
-  def display
-    File.foreach("inputdata/pipe.txt") { |line| print line } 
+  def display_pipers 
+    File.open("inputdata/pipe.txt").read
+  end
+
+  def display_commars 
+    File.open("inputdata/comma.txt").read
+  end
+
+  def display_spacers 
+    File.open("inputdata/space.txt").read
+  end
+end
+
+# blargh
+class Date
+  def initialize(params)
+    params.each do |key,value|
+      instance_variable_set("@#{key}",value)
+    end
+  end
+
+  def format
+    @dob = Date.strptime(@dob, '%m-%d-%Y')
+    @dob.gsub!(/[-]/,'/')
+    puts @dob
   end
 end
 
 class Person
   attr_accessor :lastname, :firstname, :middleinitial, :gender, :color, :dob
 
+  def initialize(params)
+    params.each do |key,value|
+      instance_variable_set("@#{key}",value)
+    end
+
+    if @gender == "F" || @gender == "Female"
+    @gender = "Female"
+    elsif @gender == "M" || @gender == "Male"
+    @gender = "Male"
+    else
+      "Whoops!  A person in your file does not have a gender."
+    end
+  end
+
   def self.from_pipe_people(pipe_people)
-    people = []
-    pipe_people.each_line do |line|
-      attributes = line.split(/ | /)
-      people << {
-      :lastname => attributes[0], 
-      :firstname => attributes[1],
-      :middleinitial => attributes[2], 
-      :gender => attributes[3], 
-      :color => attributes[4], 
-      :dob => attributes[5],
+    pipehash = []
+    pipe_people.each_line do |line|      
+      params = line.split(" | ") 
+      pipehash << {
+      :lastname => params[0], 
+      :firstname => params[1],
+      :middleinitial => params[2], 
+      :gender => params[3], 
+      :color => params[4], 
+      :dob => params[5]
       }
     end
-    return people
+    return pipehash
+  end
+
+  def self.from_comma_people(comma_people)
+    commahash = []
+    comma_people.each_line do |line|
+      params = line.split(", ")
+      commahash << {
+        :lastname => params[0],
+        :firstname => params[1],
+        :gender => params[2],
+        :color => params[3],
+        :dob => params[4]
+      }
+    end
+    return commahash
+  end
+
+  def self.from_space_people(space_people)
+    spacehash = []
+    space_people.each_line do |line|
+      params = line.split(" ")
+      spacehash << {
+        :lastname => params[0],
+        :firstname => params[1],
+        :middleinitial => params[2],
+        :gender => params[3],
+        :dob => params[4],
+        :color => params[5]
+      }
+    end
+    return spacehash
   end
 end
 
-# for array list item,regex the info out into a hash (line 13-15)
+def process(sort_order)
 
+#1. get data in --> array of hashes
+  # pipehash = Person.from_pipe_people(PeopleData.new.display_pipers)
+  # people = Person.from_comma_people(PeopleData.new.display_commars)
+
+#2. get data together --> array of hashes
+
+  peoplehash = []
+
+  peoplehash.concat(Person.from_pipe_people(PeopleData.new.display_pipers))
+  peoplehash.concat(Person.from_comma_people(PeopleData.new.display_commars))
+  peoplehash.concat(Person.from_space_people(PeopleData.new.display_spacers))
+  
+#3. create Persons 
+
+  person = peoplehash.map do |peoplehash|
+    Person.new(peoplehash)
+  end
+
+  if sort_order == "gender" 
+    person.sort_by! { |personentry| [personentry.gender, personentry.lastname] }
+    person.each do |personentry|
+      puts personentry.inspect
+    end
+
+  elsif sort_order == "birthdate"
+    person.sort_by! { |personentry| personentry.dob }
+    person.each do |personentry|
+      puts personentry.inspect
+    end
+
+  elsif sort_order == "lastname"
+    person.sort_by! { |personentry| personentry.lastname }
+    person.reverse!
+    person.each do |personentry|
+      puts personentry.inspect
+    end
+    
+  else
+    puts "Howdy! Please type a sort order: gender, birthdate, or lastname."
+  end
+
+  person.each do |personentry|
+    puts "#{personentry.lastname} #{personentry.firstname} #{personentry.gender} #{personentry.dob} #{personentry.color}"
+  end
+end
+
+process ARGV[0]
+
+
+
+
+
+
+# for array list item,regex the info out into a hash (line 13-15)
--- test/test.rb
+++ test/test.rb
@@ -2,70 +2,173 @@ require 'minitest/spec'
 require 'minitest/autorun'
 require_relative '../app/people'
 
-describe PeopleData do
-  it "should be created" do
-    PeopleData.new.must_be_instance_of PeopleData
+# large acceptance test
+describe "the script" do
+  it "should process the data sorted by females before males last name ascending when 'gender' is passed to the script" do
+    proc { process "gender" }.must_output("Hingis Martina Female 4/2/1979 Green
+Kelly Sue Female 7/12/1959 Pink
+Kournikova Anna Female 6/3/1975 Red
+Seles Monica Female 12/2/1973 Black
+Abercrombie Neil Male 2/13/1943 Tan
+Bishop Timothy Male 4/23/1967 Yellow
+Bonk Radek Male 6/3/1975 Green
+Bouillon Francis Male 6/3/1975 Blue
+Smith Steve Male 3/3/1985 Red
+")
   end
 
-  it "should print the contents of a given file" do
-    proc { PeopleData.new.display }.must_output("Smith | Steve | D | M | Red | 3-3-1985
-Bonk | Radek | S | M | Green | 6-3-1975
-Bouillon | Francis | G | M | Blue | 6-3-1975")
+  it "should process the data sorted by birthdate ascending when 'birthdate' is passed to the script" do
+    proc { process "birthdate" }.must_output("Abercrombie Neil Male 2/13/1943 Tan
+Kelly Sue Female 7/12/1959 Pink
+Bishop Timothy Male 4/23/1967 Yellow
+Seles Monica Female 12/2/1973 Black
+Bonk Radek Male 6/3/1975 Green
+Bouillon Francis Male 6/3/1975 Blue
+Kournikova Anna Female 6/3/1975 Red
+Hingis Martina Female 4/2/1979 Green
+Smith Steve Male 3/3/1985 Red
+")
+  end
+
+  it "should process the data sorted by lastname descending when 'lastname' is passed to the script" do
+    proc { process "lastname" }.must_output("Smith Steve Male 3/3/1985 Red
+Seles Monica Female 12/2/1973 Black
+Kournikova Anna Female 6/3/1975 Red
+Kelly Sue Female 7/12/1959 Pink
+Hingis Martina Female 4/2/1979 Green
+Bouillon Francis Male 6/3/1975 Blue
+Bonk Radek Male 6/3/1975 Green
+Bishop Timothy Male 4/23/1967 Yellow
+Abercrombie Neil Male 2/13/1943 Tan
+")
   end
 end
 
-describe Person do
-  it "should create 3 people from a file with 3 lines" do
-   people = Person.from_pipe_people("Smith | Steve | D | M | Red | 3-3-1985
+describe PeopleData do
+  it "should be created" do
+    PeopleData.new.must_be_instance_of PeopleData
+  end 
+
+  it "should return the contents of a given file" do
+    PeopleData.new.display_pipers.must_equal("Smith | Steve | D | M | Red | 3-3-1985
 Bonk | Radek | S | M | Green | 6-3-1975
 Bouillon | Francis | G | M | Blue | 6-3-1975")
-   people.wont_be_empty
-   people.size.must_equal 3
   end
 
-  it "should return the pipe people information for each person in an array of hashes" do
+  it "should return the contents of a given file" do
+    PeopleData.new.display_commars.must_equal("Abercrombie, Neil, Male, Tan, 2/13/1943
+Bishop, Timothy, Male, Yellow, 4/23/1967
+Kelly, Sue, Female, Pink, 7/12/1959")
+  end
+
+  it "should return the contents of a given file" do
+    PeopleData.new.display_spacers.must_equal("Kournikova Anna F F 6-3-1975 Red
+Hingis Martina M F 4-2-1979 Green
+Seles Monica H F 12-2-1973 Black")
+  end
+end
+
+
+describe Person do
+# this could be controversial in TDD as it is a test from within a class; however, given that parsing is the first step from input the 3 different files && we need to process order, this is a reasonable test to have
+  it "should return people information for each of the people in an array of hashes" do
     people = Person.from_pipe_people("Smith | Steve | D | M | Red | 3-3-1985
 Bonk | Radek | S | M | Green | 6-3-1975
 Bouillon | Francis | G | M | Blue | 6-3-1975")
     people.first[:lastname].must_equal("Smith")
   end
 
+  it "should return people information for each of the people in an array of hashes" do
+    people = Person.from_comma_people("Abercrombie, Neil, Male, Tan, 2/13/1943
+Bishop, Timothy, Male, Yellow, 4/23/1967
+Kelly, Sue, Female, Pink, 7/12/1959")
+    people.first[:lastname].must_equal("Abercrombie")
+  end
+
+  it "should return people information for each of the people in an array of hashes" do
+    people = Person.from_space_people("Kournikova Anna F F 6-3-1975 Red
+Hingis Martina M F 4-2-1979 Green
+Seles Monica H F 12-2-1973 Black")
+    people.first[:lastname].must_equal("Kournikova")
+  end
+
+#this can be combined with 1 below
+  it "should create a person with her/his attributes from his/her hash" do
+    params = {
+      :lastname => "Bouillon", 
+      :firstname => "Francis",
+      :middleinitial => "G", 
+      :gender => "M", 
+      :color => "Blue", 
+      :dob => "6-3-1975"
+      } 
+    person = Person.new(params)        
+    person.must_be_instance_of Person
+    person.lastname.must_equal("Bouillon")
+    person.firstname.must_equal("Francis")
+    person.middleinitial.must_equal("G")
+    person.color.must_equal("Blue")
+  end
+
+  # it "should create a person with her/his attributes from his/her hash" do
+  #   params = {
+  #     :lastname => "Abercrombie", 
+  #     :firstname => "Neil", 
+  #     :gender => "Male", 
+  #     :color => "Tan", 
+  #     :dob => "2/13/1943"
+  #     } 
+  #   person = Person.new(params)        
+  #   person.must_be_instance_of Person
+  #   person.lastname.must_equal("Abercrombie")
+  #   person.firstname.must_equal("Neil")
+  #   person.color.must_equal("Tan")
+  # end
+
+  it "should return gender in full text" do
+    params = {
+      :lastname => "Bouillon", 
+      :firstname => "Francis",
+      :middleinitial => "G", 
+      :gender => "M", 
+      :color => "Blue", 
+      :dob => "6-3-1975"
+    }
+    person = Person.new(params)
+    person.gender.must_equal("Male")
+  end  
+end
+
+describe Date do
+  it "should format dob into M/D/YYYY" do
+    params = {
+      :lastname => "Bouillon", 
+      :firstname => "Francis",
+      :middleinitial => "G", 
+      :gender => "M", 
+      :color => "Blue", 
+      :dob => "6-3-1975"
+    } 
+    person = Person.new(params)
+    person.dob.must_be_instance_of Date
+  end
 end
 
 
-# option1
-# - line delination as 3 objects
-# - break down each line into components (array, hash)
-# - each collection of components is a person
+# OUTPUT
+# Sort dates in the format M/D/YYYY
+# remove /n from line
+# unit test sorting
+# Overall output - @dob.gsub!(/[-]/,'/')
+#dob.strftime("%m/%d/%Y")
 
 
- 
-# (TRANSFORM) Get all three files to output
+# TRANSFORM
+# Get all three files to process the fields are in the same order (last name, first name, gender, date of birth, favorite color)
 
-# (TRANSFORM) Get all three files to output the fields are in the same order (last name, first name, gender, date of birth, favorite color)
-
-# (TRANSFORM) Display dates in the format M/D/YYYY
-
-
-
-# describe People do
-#   it "should" do
-#     People.new.must_include(other)
-#   end
-# end
-
-# get file, read file, for each instance of person create new instance of people
-
-# (INPUT) Get comma file to output records and info
-
-# (INPUT) Get space file to output records and info
+# INPUT
+# Get comma file to process records and info
+# Get space file to process records and info
 
 
 
-# Output 1 – sorted by gender (females before males) then by last name ascending
-
-# Output 2 – sorted by birth date, ascending.
-
-# Output 3 – sorted by last name, descending.
-
-# what command should we do?  1 for all options or corresponding command per option
